c939e5cd8d0b9ac5fe769f66f2fc07a6d08c3f7b
Grzesiek <grzesiekl1998@gmail.com>
2021-06-24 20:53:42 +0200

    add remove and delete user

--- src/main/java/ti/dao/UserDao.java
+++ src/main/java/ti/dao/UserDao.java
@@ -1,5 +1,6 @@
 package ti.dao;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -9,11 +10,6 @@ import ti.util.HibernateUtil;
 
 public class UserDao {
 
-    /**
-     * Save User
-     * @param user
-     * @return
-     */
     public void saveUser(User user) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
@@ -30,5 +26,44 @@ public class UserDao {
             e.printStackTrace();
         }
     }
+    public User getUserById(int user_id) {
+        Session session = null;
+        User user = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            user =  (User) session.get(User.class, user_id);
+            Hibernate.initialize(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return user;
+    }
+    public void deleteUserById(int id)
+    {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
 
-}
+            // Delete a persistent object
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    }
+
+
+
